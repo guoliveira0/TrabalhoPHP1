8b0dfb405bfeb1e444f1a17af63007fdfc8b9355
Luiz Gustavo <luizgustavo160206@gmail.com>
2023-08-03 16:29:52 -0300

    criandoScriptDeSubmit

--- question1.php
+++ question1.php
@@ -15,11 +15,11 @@
     <main>
         <h1>Trabalho: Questão 1</h1>
         <form action="question1.php" method="get" id="Form" name="Form">
-        
+
             <fieldset>
                 <legend>Critérios para geração</legend>
                 <label for="quant">Quantidade de elementos: </label>
-                <input type="number" name="quant" id="quant" min=1 max=15 value="<?php echo isset($_GET['quant']) ? htmlspecialchars($_GET['quant']) : '1'; ?>" title="De 1 a 15" ><br>
+                <input type="number" name="quant" id="quant" min=1 max=15 value="<?php echo isset($_GET['quant']) ? htmlspecialchars($_GET['quant']) : '1'; ?>" title="De 1 a 15"><br>
 
                 <input type="radio" name="elements" id="text" value=text <?php echo (isset($_GET['elements']) && $_GET['elements'] == 'text') ? 'checked' : ''; ?> onclick="sendForm()">
                 <label for="text">Texto</label><br>
@@ -27,7 +27,7 @@
                 <input type="radio" name="elements" id="password" value="password" <?php echo (isset($_GET['elements']) && $_GET['elements'] == 'password') ? 'checked' : ''; ?> onclick="sendForm()">
                 <label for="password">Senha</label><br>
 
-                <input type="radio" name="elements" id="button" value="button"  <?php echo (isset($_GET['elements']) && $_GET['elements'] == 'button') ? 'checked' : ''; ?> onclick="sendForm()">
+                <input type="radio" name="elements" id="button" value="button" <?php echo (isset($_GET['elements']) && $_GET['elements'] == 'button') ? 'checked' : ''; ?> onclick="sendForm()">
                 <label for="button">Botão</label><br>
 
                 <input type="radio" name="elements" id="radio" value="radio" <?php echo (isset($_GET['elements']) && $_GET['elements'] == 'radio') ? 'checked' : ''; ?> onclick="sendForm()">
@@ -38,7 +38,16 @@
 
                 <input type="radio" name="elements" id="range" value="range" <?php echo (isset($_GET['elements']) && $_GET['elements'] == 'range') ? 'checked' : ''; ?> onclick="sendForm()">
                 <label for="range">Faixa</label><br>
-               
+
 
             </fieldset>
         </form>
+        <script>
+            function sendForm() {
+                document.getElementById("Form").submit();
+            }
+        </script>
+        <a href="index.php">Página inicial</a>
+    </main>
+    <footer></footer>
+</body>
