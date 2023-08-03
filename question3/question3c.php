e5779d9928093e6b256c7576209ffe08d718ace2
Luiz Gustavo <luizgustavo160206@gmail.com>
2023-08-03 17:20:26 -0300

    criandopage3question03

---
+++ question3/question3c.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Questão 3</title>
+</head>
+<body>
+    <header>
+        <h1>Desenvolvimento Web</h1>
+    </header>
+    <main>
+        <h1>Trabalho: Questão 3</h1>
+        <h3>Q3. Lorem ipsum dolor sit amet.</h3>
+        <?php
+         $question2 = "question1={$_GET['question1']}&question2={$_GET['question2']}";
+         echo("
+        <p><a href=\"question3d.php?$question2&question3=A\">    A&#41;A Lorem ipsum dolor sit amet.</a></p>
+        <p><a href=\"question3d.php?$question2&question3=B\">	B&#41;A Lorem ipsum dolor sit amet.</a></p>
+        <p><a href=\"question3d.php?$question2&question3=C\">	C&#41;A Lorem ipsum dolor sit amet.</a></p>
+        <p><a href=\"question3d.php?$question2&question3=D\">	D&#41;A Lorem ipsum dolor sit amet.</a></p>
+        <p><a href=\"question3d.php?$question2&question3=E\"style=\"color:green\">	E&#41;A Lorem ipsum dolor sit amet.</a></p>"
+         );
+        ?>
+         <p><a href="question3.php">Cancelar</a></p>
+         <p><a href="../index.php">Página Incial</a></p>
+    </main>
+    <footer>
+    </footer>
+</body>
+</html>
