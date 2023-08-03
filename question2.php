e4c6bf7c5d1a0831479440845f0073b5bfefb311
fernanda812 <fernandasoaresnunes128@gmail.com>
2023-08-03 17:04:36 -0300

    criandoHTMLQuestion2

---
+++ question2.php
@@ -0,0 +1,28 @@
+!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Questão 2</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <h1>Desenvolvimento Web</h1>
+    </header>
+    <main>
+        <h1>Trabalho: Questão 2</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th></th>
+                    </tbody>
+        </table>
+        <a href="index.php">Página Principal</a>
+    </main>
+    <footer></footer>
+</body>
+
+</html>
