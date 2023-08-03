a98edebe2bd93eba3f3c278ada41616dd44f0f08
Luiz Gustavo <luizgustavo160206@gmail.com>
2023-08-03 17:18:34 -0300

    criandoHTMLpage2questao03

---
+++ question3/question3b.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Questão 3</title>
+</head>
+
+<body>
+    <header>
+        <h1>Desenvolvimento Web</h1>
+    </header>
+    <main>
+        <h1>Trabalho: Questão 3</h1>
+        <h3>Q2. Lorem ipsum dolor sit amet.</h3>
+        <p><a href="question3.php">Cancelar</a></p>
+        <p><a href="../index.php">Página Incial</a></p>
+    </main>
+    <footer>
+    </footer>
+</body>
+
+</html>
