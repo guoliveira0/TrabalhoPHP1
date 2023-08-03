29a5ea631911c1e3958eb0718f2dcb4b80086d59
Luiz Gustavo <luizgustavo160206@gmail.com>
2023-08-03 17:15:54 -0300

    criandoprimeirapage03

---
+++ question3/question3.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Questão 3</title>
+</head>
+<body>
+    <header>
+        <h1>Desenvolvimento Web</h1>
+    </header>
+    <main>
+        <h1>Trabalho: Questão 3</h1>
+         <h3>Q1. Lorem ipsum dolor sit amet.</h3>
+        <p><a href="question3b.php?question1=A" style="color:green;">    A&#41;A Lorem ipsum dolor sit amet.</a></p>
+        <p><a href="question3b.php?question1=B">	B&#41;A Lorem ipsum dolor sit amet.</a></p>
+        <p><a href="question3b.php?question1=C">	C&#41;A Lorem ipsum dolor sit amet.</a></p>
+        <p><a href="question3b.php?question1=D">	D&#41;A Lorem ipsum dolor sit amet.</a></p>
+        <p><a href="question3b.php?question1=E">	E&#41;A Lorem ipsum dolor sit amet.</a></p>
+        <p><a href="question3.php">Cancelar</a></p>
+        <p><a href="../index.php">Página Incial</a></p>
+    </main>
+    <footer>
+    </footer>
+</body>
+</html>
