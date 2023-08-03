a8fe3a8e62fa74878b00c3ad43b50cc0f6797ecf
Luiz Gustavo <luizgustavo160206@gmail.com>
2023-08-03 17:19:14 -0300

    criandoPHPpage2questao03

--- question3/question3b.php
+++ question3/question3b.php
@@ -14,6 +14,16 @@
     <main>
         <h1>Trabalho: Questão 3</h1>
         <h3>Q2. Lorem ipsum dolor sit amet.</h3>
+        <?php
+        $question1 = "question1={$_GET['question1']}";
+        echo ("
+        <p><a href=\"question3c.php?$question1&question2=A\">    A&#41;A Lorem ipsum dolor sit amet.</a></p>
+        <p><a href=\"question3c.php?$question1&question2=B\">	B&#41;A Lorem ipsum dolor sit amet.</a></p>
+        <p><a href=\"question3c.php?$question1&question2=C\" style=\"color:green\">	C&#41;A Lorem ipsum dolor sit amet.</a></p>
+        <p><a href=\"question3c.php?$question1&question2=D\">	D&#41;A Lorem ipsum dolor sit amet.</a></p>
+        <p><a href=\"question3c.php?$question1&question2=E\">	E&#41;A Lorem ipsum dolor sit amet.</a></p>"
+        );
+        ?>
         <p><a href="question3.php">Cancelar</a></p>
         <p><a href="../index.php">Página Incial</a></p>
     </main>
