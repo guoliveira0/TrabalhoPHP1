a1ccea0e17bf7792efdf18c46778f7df41ad9643
fernanda812 <fernandasoaresnunes128@gmail.com>
2023-08-03 17:53:58 -0300

    consertandopage04

--- question4.php
+++ question4.php
@@ -22,7 +22,7 @@
                 <label for="period">Período(Meses):</label>
                 <input type="number" name="period" id="period" min=1 max=480 title="Entre 1 e 480" value="<?php echo isset($_GET['period']) ? htmlspecialchars($_GET['period']) : '0'; ?>"><br>
                 <label for="income">Rendimento(%):</label>
-                <input type="number" name="income" id="income" min=0 max=20 step="0.01" title="Até 20%" value="<?php echo isset($_GET['income']) ? htmlspecialchars($_GET['income']) : '1'; ?>"><br>
+                <input type="number" name="income" id="income" min=0 max=20 step="0.01" title="Até 20%" value="<?php echo isset($_GET['income']) ? htmlspecialchars($_GET['income']) : '0'; ?>"><br>
                 <label for="contribution">Aporte mensal(R$):</label>
                 <input type="number" name="contribution" id="contribution" min=0 max=999999.99 step="0.01" title="Máximo de 999999.99" value="<?php echo isset($_GET['contribution']) ? htmlspecialchars($_GET['contribution']) : '0'; ?>"><br>
                 <input type="submit" value="Processar">
