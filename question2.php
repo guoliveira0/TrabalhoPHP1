ebe1f3bae8f70f2df9985729115b93bb2b788975
fernanda812 <fernandasoaresnunes128@gmail.com>
2023-08-03 17:07:24 -0300

    criandotbody

--- question2.php
+++ question2.php
@@ -25,8 +25,47 @@
                     ?>
                 </tr>
             </thead>
+            <tbody>
+                <?php
+                for ($y = 146; $y < 210 + 1; $y++) {
+                    echo ("<tr>");
+                    echo ("<td>$y</td>");
+                    for ($z = 46; $z < 120 + 1; $z++) {
+                        $imc = $z / (($y * 0.01) * ($y * 0.01));
+                        $convertImc = round($imc, 2);
+                        switch (true) {
+                            case ($imc >= 0 && $imc < 16):
+                                echo ("<td class=\"veryLowGrave\" title=\"Baixo muito grave &#013;$y cm&#013;$z kg&#013;$convertImc\"></td>");
+                                break;
+                            case ($imc >= 16 && $imc < 17):
+                                echo ("<td class=\"veryLow\" title=\"Muito Grave &#013;$y cm&#013;$z kg&#013;$convertImc\"></td>");
+                                break;
+                            case ($imc >= 17 && $imc < 18.5):
+                                echo ("<td class=\"low\" title=\"Baixo &#013;$y cm&#013;$z kg&#013;$convertImc\"></td>");
+                                break;
+                            case ($imc >= 18.5 && $imc < 25):
+                                echo ("<td class=\"ideal\" title=\"Ideal &#013;$y cm&#013;$z kg&#013;$convertImc\"></td>");
+                                break;
+                            case ($imc >= 25 && $imc < 30):
+                                echo ("<td class=\"overWeight\" title=\"Sobrepeso &#013;$y cm&#013;$z kg&#013;$convertImc\"></td>");
+                                break;
+                            case ($imc >= 30 && $imc < 35):
+                                echo ("<td class=\"obesityI\" title=\"ObesidadeI &#013;$y cm&#013;$z kg&#013;$convertImc\"></td>");
+                                break;
+                            case ($imc >= 35 && $imc < 40):
+                                echo ("<td class=\"obesityII\" title=\"ObesidadeII &#013;$y cm&#013;$z kg&#013;$convertImc\"></td>");
+                                break;
+                            case ($imc >= 40):
+                                echo ("<td class=\"obesityIII\" title=\"ObesidadeIII &#013;$y cm&#013;$z kg&#013;$convertImc\"></td>");
+                                break;
+                        }
+                    }
+                    echo ("</tr>");
+                }
+                ?>
+            </tbody>
 
-            
+
         </table>
         <a href="index.php">Página Principal</a>
     </main>
